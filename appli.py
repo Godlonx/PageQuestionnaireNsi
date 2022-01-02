4d4339fbb0bc48cf93960c3ef10f3f0b21ee3e17
JeSuisTropUwU <lochlangui@gmail.com>
2022-01-02 17:29:05 +0100

    Randomisation
    Ajout d'un système de random a l'ordre des réponses et id de la bonne réponse.

--- appli.py
+++ appli.py
@@ -17,3 +17,32 @@ def questionnaire():
 
 if __name__ == '__main__':
     application.run(debug=True)
+
+
+
+from sqlite3 import *
+from random import *
+
+
+def ordre_random(idq):
+    sqliteConnection = connect('siteweb.db')
+    cursor = sqliteConnection.cursor()
+    req = randompos()
+    val = cursor.execute(req[0],idq)
+
+    sqliteConnection.commit()
+    cursor.close()
+
+    return (val, req[1])
+
+
+def randompos():
+    req1 = """SELECT bonne, fausse1, fausse2, fausse3 FROM Question WHERE id = ?;"""
+    req2 = """SELECT fausse1, bonne, fausse2, fausse3 FROM Question WHERE id = ?;"""
+    req3 = """SELECT fausse1, fausse2, bonne, fausse3 FROM Question WHERE id = ?;"""
+    req4 = """SELECT fausse1, fausse2, fausse3, bonne FROM Question WHERE id = ?;"""
+    reqn = [req1, req2, req3, req4]
+    idb = randint(0,3)
+    req = reqn[idb]
+
+    return (req, idb)
