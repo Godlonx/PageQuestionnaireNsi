07b1782ef0064a5c32d7f8f8c2960155e59c0197
Godlonx <91208870+Godlonx@users.noreply.github.com>
2022-01-30 19:46:06 +0100

    Update
    LE TIMERRRRRRRR !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Par serge Harguez

--- static/script.js
+++ static/script.js
@@ -6,3 +6,24 @@ const list = document.querySelectorAll('.list');
         }
         list.forEach((item) =>
         item.addEventListener('click', activeLink))
+
+
+let time;
+let fin;
+let temps;
+let min;
+let sec;
+let msec;
+
+function debut_timer(){
+    time = Date.now();
+}
+
+function fin_timer(){
+    min = String(~~((~~((Date.now() - time)/1000))/60))
+    sec = String(~~((Date.now() - time)/1000)%60)
+    msec = String((Date.now() - time)%1000)
+    fin = min+","+sec+","+msec
+
+    console.log(fin)
+}
